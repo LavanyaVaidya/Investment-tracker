3dc51fb99fd5c9cc04d02d02e4050e32af58669d
1192842 <mkaurkohli7@gmail.com>
2023-09-25 08:53:23 +0000

    Update investment-tracker/src/Components/Login-Register/lr.css, investment-tracker/src/Components/Login-Register/SignInUp.js, investment-tracker/src/App.js, investment-tracker/src/App.css files Deleted investment-tracker/src/Login-Register/Components/SignIn.js

--- investment-tracker/src/App.js
+++ investment-tracker/src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import SignIn from './Components/Login-register/SignIn';
+import SignInUp from './Components/Login-register/SignInUp';
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <div>
-        <SignIn></SignIn>
-      </div>
-    </div>
+    <SignInUp></SignInUp>
   );
 }
 
