f82c7b1642e8d8cb488fc173616297f3ae50491e
1192842 <mkaurkohli7@gmail.com>
2023-10-04 09:13:42 +0530

    Side-bar

--- investment-tracker/src/Frontend/Components2/Home2.js
+++ investment-tracker/src/Frontend/Components2/Home2.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import Welcome from './Welcome'
-import { useParams } from 'react-router-dom';
-import NavbarLogin from './NavbarLogin/NavbarLogin';
+import {useParams} from 'react-router-dom'
+import NavbarLogin from './NavbarLogin/NavbarLogin'
+import {useState, useRef, useEffect} from 'react'
+import AppLayout from './Side-bar/layout/AppLayout'
 
 const Home2 = () => {
-  const {user}=useParams();
-  const navHeading='Welcome '+user+' !'
+  const [isAppLayoutVisible,setAppLayoutVisible]=useState(false);
+  const appLayoutRef=useRef(null);
+  const toggleAppLayout=()=>{
+    setAppLayoutVisible(!isAppLayoutVisible);
+  }
+  useEffect(()=>
+  {
+      const handleClickOutside=(event)=>{
+        if(appLayoutRef.current && !appLayoutRef.current.contains(event.target))
+        {
+          setAppLayoutVisible(false);
+        }
+      };
+      document.addEventListener('mousedown',handleClickOutside);
+      return()=>
+      {
+        document.removeEventListener('mousedown',handleClickOutside);
+      };
+  },[]);
+  const { user } = useParams();
+
+  const navHeading='Welcome' +user+ ' !'
   return (
     <div>
       <NavbarLogin heading={navHeading}/>
       <h2> Hello guys</h2>
         <Welcome></Welcome>
+      <button className='toggle button' onClick={toggleAppLayout}>Click Me</button>
+      {isAppLayoutVisible && <div ref={appLayoutRef}><AppLayout/></div>}
     </div>
-  )
+  );
 }
 
-export default Home2
+export default Home2;
