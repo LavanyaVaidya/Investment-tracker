37a5a0d80990221c3f58574137e81ab7b45f2f1c
1192842 <mkaurkohli7@gmail.com>
2023-10-09 16:49:45 +0530

    backend login register-mongodb-springboot

---
+++ backend/ended/ended/src/main/java/com/loginregisterMSms/ended/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.loginregisterMSms.ended.repository;
+
+import com.loginregisterMSms.ended.model.CustomUser;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.bson.types.ObjectId;
+
+public interface UserRepository extends MongoRepository<CustomUser,Integer> {
+}
